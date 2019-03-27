571af7e99c7317ea79a8cf47a6e400ca7422bf64
josep.lajara <joseplajara7@gmail.com>
2019-03-27 17:36:07 +0100

    Estructura de front creada

--- EscudosJPA/Prueba-front/src/app/app.module.ts
+++ EscudosJPA/Prueba-front/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PocionModule } from './entities/pocion/pocion.module';
+import { PocionComponent } from './entities/pocion/pocion/pocion.component';
+import { PocionListComponent } from './entities/pocion/pocion-list/pocion-list.component';
+import { PocionGridComponent } from './entities/pocion/pocion-grid/pocion-grid.component';
+import { PocionDetailComponent } from './entities/pocion/pocion-detail/pocion-detail.component';
+import { HomeComponent } from './shared/home/home.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PocionComponent,
+    PocionListComponent,
+    PocionGridComponent,
+    PocionDetailComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PocionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
